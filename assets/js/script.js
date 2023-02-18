3548ab51864b1c402e0f28e3327ed662eea5b3d6
Jose Del Valle <jdelvalle88@live.com>
2023-02-17 23:51:53 -0500

    edited a code for the city to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,12 @@ function getHistory() {
 
 function renderWeather(city, weather) {
 console.log(weather);
-	
+
+
 	var date = dayjs().format('M/D/YYYY')
 	var resultsContainer = document.querySelector("#weather-results");
 
-	var city = document.createElement("h1");
+	var city = document.createElement("h1").innerHTML = `${city}` + ' ' + `${date}`;
 	city.textContent = `${city}` + `${date}`;
 	resultsContainer.append(city);
 
@@ -71,39 +72,63 @@ console.log(weather);
 };
 
 
-function renderForecast(city, forecast) {
+function renderForecast(forecast) {
 	console.log(forecast); 
-
+	var forecastDisplayEl = document.querySelector(".forecast-results");
 	// var day1 = data.daily[1].temp;
 	// var day1 = data.daily[1].humidity;
 	// var day1 = data.daily[1].wind.speed;
 	// var day1 = data.daily[1].weather[0].icon;
 
 
-	var date = dayjs().format('M/D/YYYY');
+	var fday1 = dayjs().format('M/D/YYYY');
 	var resultsContainer = document.querySelector(".weather-results");
 
-	var city = document.createElement("h1");
-	city.textContent = `${city}` + `${date}`;
-	resultsContainer.append(city);
+	var fday1 = document.createElement("h1").innerHTML = `${fday1}` + `${fday1}`;
+	// city.textContent = `${city}` + `${date}`;
+	// resultsContainer.append(city);
 
-	var temp = document.createElement("p");
-	temp.textContent = "Temp:" + forecast.main.temp + "F";
-	resultsContainer.append(temp);
+	var ftemp1 = document.createElement("p");
+	ftemp1.textContent = "Temp:" + forecast.main.temp + "F";
+	resultsContainer.append(ftemp1);
 
-	var humidity = document.createElement("p");
-	humidity.textContent = "Humidity:" + forecast.main.humidity + "%";
-	resultsContainer.append(humidity);
+	var fhumidity1 = document.createElement("p");
+	fhumidity1.textContent = "Humidity:" + forecast.main.humidity + "%";
+	resultsContainer.append(fhumidity1);
 
-	var wind = document.createElement("p");
-	wind.textContent = "Wind Speed:" + forecast.wind.speed + "mph";
-	resultsContainer.append(wind);
+	var fwind1 = document.createElement("p");
+	fwind1.textContent = "Wind Speed:" + forecast.wind.speed + "mph";
+	resultsContainer.append(fwind1);
 
 	var iconURL = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-	var icon = document.createElement("img");
-	icon.setAttribute('src', iconURL) ;
-	resultsContainer.append(icon);
+	var ficon1 = document.createElement("img");
+	ficon1.setAttribute('src', iconURL) ;
+	resultsContainer.append(ficon1);
 	
+
+	var fday2 = dayjs().format('M/D/YYYY');
+	var resultsContainer = document.querySelector(".weather-results");
+
+	var fday2 = document.createElement("h1").innerHTML = `${fday2}`;
+	// city.textContent = `${city}` + `${date}`;
+	// resultsContainer.append(city);
+
+	var ftemp2 = document.createElement("p");
+	ftemp2.textContent = "Temp:" + forecast.main.temp + "F";
+	resultsContainer.append(ftemp2);
+
+	var fhumidity2 = document.createElement("p");
+	fhumidity2.textContent = "Humidity:" + forecast.main.humidity + "%";
+	resultsContainer.append(fhumidity2);
+
+	var fwind2 = document.createElement("p");
+	fwind2.textContent = "Wind Speed:" + forecast.wind.speed + "mph";
+	resultsContainer.append(fwind2);
+
+	var iconURL = `https://openweathermap.org/img/w/${weather.weather[1].icon}.png`;
+	var ficon2 = document.createElement("img");
+	ficon2.setAttribute('src', iconURL) ;
+	resultsContainer.append(ficon2);
 	
 };
 
@@ -113,8 +138,8 @@ function renderItems(city, data) {
 	renderWeather(city, data.list[0], data.city.timeZone);
 
 	//call function to render forecast
-	renderForecast(city, data.list[0], data.city.timeZone);
-	
+	renderForecast(data.list[1], data.timeZone);
+	renderForecast(data.list[2], data.timeZone);
 	// forecast.eachDay(days) 
 	// 	var date = days().format('M/D/YYYY');
 	// 	var days = [];
